50cbcdfe084e0847dbf84158b1df15b2b2092327
twizard <trevcoons@gmail.com>
2021-07-01 19:17:27 -0700

    connect save button to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,17 +24,15 @@ function setPlanner() {
 }
 
 
-// in order to access the local storage item we need to load the saved data
-// load each hour using the method getItem
-$('#hour-9 .description').val(localStorage.getItem('hour-9'));
-$('#hour-10 .description').val(localStorage.getItem('hour-10'));
-$('#hour-11 .description').val(localStorage.getItem('hour-11'));
-$('#hour-12 .description').val(localStorage.getItem('hour-12'));
-$('#hour-13 .description').val(localStorage.getItem('hour-13'));
-$('#hour-14 .description').val(localStorage.getItem('hour-14'));
-$('#hour-15 .description').val(localStorage.getItem('hour-15'));
-$('#hour-16 .description').val(localStorage.getItem('hour-16'));
-$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+setPlanner();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
 // change color to show time past, present and future
 function timeUpdate() {
 // getting moment hours in military time 
